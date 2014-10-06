22c539667ab3ee1a4701888c7e42428eb0a66fa0
Emil Virkki <emil.virkki@gmail.com>
2014-10-06 23:46:05 +0300

    Added master module in installation

--- fj16-dev-setup.sh
+++ fj16-dev-setup.sh
@@ -2,3 +2,7 @@
 
 # Install Drupal
 drush si minimal -y --account-name=admin --account-pass=admin --site-name="Roihu 2016" --db-url=sqlite://sites/default/files/.db.sqlite
+
+# Enable all fj16 features
+cd ./sites/default
+drush en fj16_master -y
