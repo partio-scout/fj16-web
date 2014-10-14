4da6f1340d6372025d7261247a7078ed459bb83b
Emil Virkki <emil.virkki@gmail.com>
2014-10-15 00:09:57 +0300

    Added dev modules and localization update to dev install

--- fj16-dev-setup.sh
+++ fj16-dev-setup.sh
@@ -6,3 +6,10 @@ drush si minimal -y --account-name=admin --account-pass=admin --site-name="Roihu
 # Enable all fj16 features
 cd ./sites/default
 drush en fj16_master -y
+
+# Enable dev modules
+drush en field_ui -y
+drush en views_ui -y
+
+# Download translations
+drush l10n_update -y
