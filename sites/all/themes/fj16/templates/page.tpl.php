cd38473b35a27dd7cb8d4b28624ce8ea5ca6830b
Ville Vuorenmaa <villevuor@gmail.com>
2014-10-17 21:07:36 +0300

    Desktop header styles

--- sites/all/themes/fj16/templates/page.tpl.php
+++ sites/all/themes/fj16/templates/page.tpl.php
@@ -73,23 +73,17 @@
  */
 ?>
 
-  <div id="page-wrapper"><div id="page">
+  <div id="page">
 
-    <div id="header"><div class="section clearfix">
+    <div id="header">
 
-      <div id="header-left">
-        <?php print render($page['header_left']); ?>
+      <div id="bg-slides">
+        <div class="slide slide1"></div>
       </div>
 
-      <div id="header-right">
-      <?php print render($page['header_right']); ?>
+      <div id="top-bar">
+        <?php print theme('links__system_main_menu', array('links' => menu_navigation_links('menu-social-media-links'))); ?>
       </div>
-
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
         <div id="name-and-slogan">
@@ -108,23 +102,21 @@
           <?php if ($site_slogan): ?>
             <div id="site-slogan"><?php print $site_slogan; ?></div>
           <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
+        </div>
       <?php endif; ?>
 
-      <?php print render($page['header']); ?>
 
-    </div></div> <!-- /.section, /#header -->
+      <?php if ($main_menu): ?>
+        <div id="navigation">
+          <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu'))); ?>
+        </div>
+      <?php endif; ?>
 
-    <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-      </div></div> <!-- /.section, /#navigation -->
-    <?php endif; ?>
+    </div>
 
-    <?php if ($breadcrumb): ?>
+    <?php /*if ($breadcrumb): ?>
       <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
+    <?php endif;*/ ?>
 
     <?php print $messages; ?>
 
@@ -140,12 +132,12 @@
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
         <?php print $feed_icons; ?>
-      </div></div> <!-- /.section, /#content -->
+      </div></div>
 
-    </div></div> <!-- /#main, /#main-wrapper -->
+    </div></div>
 
-    <div id="footer"><div class="section">
+    <div id="footer">
       <?php print render($page['footer']); ?>
-    </div></div> <!-- /.section, /#footer -->
+    </div
 
-  </div></div> <!-- /#page, /#page-wrapper -->
+  </div>
