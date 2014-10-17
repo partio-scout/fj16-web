ddd18787887fc703549e09fd4100a10740a2337c
Ville Vuorenmaa <villevuor@gmail.com>
2014-10-17 21:21:21 +0300

    Simple footer styles

--- sites/all/themes/fj16/templates/page.tpl.php
+++ sites/all/themes/fj16/templates/page.tpl.php
@@ -120,9 +120,9 @@
 
     <?php print $messages; ?>
 
-    <div id="main-wrapper"><div id="main" class="clearfix">
+    <div id="main" class="clearfix">
 
-      <div id="content" class="column"><div class="section">
+      <div id="content" class="column">
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
@@ -132,12 +132,16 @@
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
         <?php print $feed_icons; ?>
-      </div></div>
+      </div>
 
-    </div></div>
+    </div>
 
     <div id="footer">
-      <?php print render($page['footer']); ?>
-    </div
+      <div class="container">
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="roihu-logo"><?php print $site_name; ?></a>
+        <?php print render($page['footer']); ?>
+        <a href="http://partio.fi" target="_blank" id="sp-logo">Suomen Partiolaiset – Finlands Scouter ry</a>
+      </div>
+    </div>
 
   </div>
