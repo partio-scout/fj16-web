fc77e2b3653bd2d7d92b4561b266124935aaf76d
Emil Virkki <emil.virkki@gmail.com>
2014-10-19 15:58:07 +0300

    Changed deploy script to use absolute path to Drush

--- fj16-deploy.sh
+++ fj16-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "DEPLOYING..."
-drush updatedb -y
-drush features-revert-all -y
-drush cc all #clear caches
+/var/lib/nginx/drush/drush updatedb -y
+/var/lib/nginx/drush/drush features-revert-all -y
+/var/lib/nginx/drush/drush cc all #clear caches
 echo "DEPLOY DONE"
