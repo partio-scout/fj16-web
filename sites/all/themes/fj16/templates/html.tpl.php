2c0c793605df7beb2336bbd96e64b580d21cdc02
Ville Vuorenmaa <villevuor@gmail.com>
2014-10-21 18:36:39 +0300

    Favicons added

--- sites/all/themes/fj16/templates/html.tpl.php
+++ sites/all/themes/fj16/templates/html.tpl.php
@@ -51,6 +51,23 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="apple-touch-icon" sizes="57x57" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/sites/all/themes/fj16/img/favicons/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="/sites/all/themes/fj16/img/favicons/favicon-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="/sites/all/themes/fj16/img/favicons/favicon-160x160.png" sizes="160x160">
+  <link rel="icon" type="image/png" href="/sites/all/themes/fj16/img/favicons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="/sites/all/themes/fj16/img/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="/sites/all/themes/fj16/img/favicons/favicon-32x32.png" sizes="32x32">
+  <meta name="msapplication-TileColor" content="#e8cda8">
+  <meta name="msapplication-TileImage" content="/sites/all/themes/fj16/img/favicons/mstile-144x144.png">
+
   <?php print $styles; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
