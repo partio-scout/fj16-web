10ed43365b163cc9de8059b79726b7a3d3bd2710
Emil Virkki <emil.virkki@gmail.com>
2014-10-23 00:08:33 +0300

    Better title for front page

--- sites/all/themes/fj16/template.php
+++ sites/all/themes/fj16/template.php
@@ -2,6 +2,6 @@
 
 function fj16_preprocess_html(&$vars) {
   if(drupal_is_front_page()) {
-  	$vars['head_title']	= check_plain(variable_get('site_name'));
+  	$vars['head_title']	= check_plain(variable_get('site_name') . ' - ' . variable_get('site_slogan'));
   }
 }
