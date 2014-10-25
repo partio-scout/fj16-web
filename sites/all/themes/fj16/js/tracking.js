986ca45fb43a55b9faeeb703eec70a043b344b1d
Ville Vuorenmaa <villevuor@gmail.com>
2014-10-25 09:35:38 +0300

    JS bug fix

--- sites/all/themes/fj16/js/tracking.js
+++ sites/all/themes/fj16/js/tracking.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function() {
 
 	// Send event to google analytics
 	function send_ga_events(data_category,data_action,data_label){
@@ -13,27 +13,27 @@ jQuery(document).ready(function ($) {
 		init : function() {
 
 			// GENERAL TRACKING THROUGH DATA-ATTRIBUTES ON LINKS
-			$('a[data-ga-track="event"]').on('click', function(e) {
-				var data_category = $(this).attr('data-ga-category');
-				var data_action = $(this).attr('data-ga-action');
-				var data_label = $(this).attr('data-ga-opt_label');
+			jQuery('a[data-ga-track="event"]').on('click', function(e) {
+				var data_category = jQuery(this).attr('data-ga-category');
+				var data_action = jQuery(this).attr('data-ga-action');
+				var data_label = jQuery(this).attr('data-ga-opt_label');
 					// add page title in label, use swiftype page title
-					page_title = $('meta[property="st:title"]').attr("content");
+					page_title = jQuery('meta[property="st:title"]').attr("content");
 					data_label = data_label + ' / ' + page_title;
 				send_ga_events(data_category,data_action,data_label );
 			}); 
 
-			$('#navigation #main-menu a').on('click', function(e) {
+			jQuery('#navigation #main-menu a').on('click', function(e) {
 				var data_category = 'Main navigation';
 				var data_action = 'Click';
-				var data_label = $(this).html();
+				var data_label = jQuery(this).html();
 				send_ga_events(data_category,data_action,data_label);
 			}); 
 		
-			$('#footer a').on('click', function(e) {
-				var data_label = $(this).html();
+			jQuery('#footer a').on('click', function(e) {
+				var data_label = jQuery(this).html();
 
-				if($(this).attr('id') == 'sp-logo') {
+				if(jQuery(this).attr('id') == 'sp-logo') {
 					data_label = 'SP logo';
 				}
 
@@ -44,7 +44,7 @@ jQuery(document).ready(function ($) {
 	}
 
 
-	$.fn.app2 = function(scope, method) {
+	jQuery.fn.app2 = function(scope, method) {
 		if (app2[scope][method]) {
 			return app2[scope][method].apply( this, Array.prototype.slice.call( arguments, 1 ));
 		} else if ( typeof method === 'object' || ! method ) {
@@ -54,5 +54,5 @@ jQuery(document).ready(function ($) {
 		}
 	};
 
-	$('body').app2('tracking');
+	jQuery('body').app2('tracking');
 });
