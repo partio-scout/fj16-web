44d9bedb84d368b6901871ff69c4aa165dc7e7b5
Ville-Pekka Reponen <vpreponen@gmail.com>
2015-10-13 23:34:45 +0300

    fix countdown js workaround

--- sites/all/modules/custom/fj16_countdown/js/fj16_countdown.js
+++ sites/all/modules/custom/fj16_countdown/js/fj16_countdown.js
@@ -1,33 +1,31 @@
-(function ($) {
-	setTimeout(function(){
-		var countdownClock = $('div.countdown-clock');	
-		var countdownDate = countdownClock.data('date').split('-');
-		var countdownDays = Drupal.t('days');
-		var countdownHours = Drupal.t('hours');
-		var countdownMinutes = Drupal.t('minutes');
-		var countdownSeconds = Drupal.t('seconds');
-		
-		countdownDate = new Date(countdownDate[0],countdownDate[1]-1,countdownDate[2]);
+jQuery(function($) {
+	var countdownClock = $('div.countdown-clock');	
+	var countdownDate = countdownClock.data('date').split('-');
+	var countdownDays = Drupal.t('days');
+	var countdownHours = Drupal.t('hours');
+	var countdownMinutes = Drupal.t('minutes');
+	var countdownSeconds = Drupal.t('seconds');
+	
+	countdownDate = new Date(countdownDate[0],countdownDate[1]-1,countdownDate[2]);
 
-		$('div.countdown-clock').append('<div id="clock"><p><span id="daysLeft"></span> '+countdownDays+'</p><div class="space">:</div><p><span id="hours"></span> '+countdownHours+'</p><div class="space">:</div><p><span id="minutes"></span> '+countdownMinutes+'</p><div class="space">:</div><p><span id="seconds"></span> '+countdownSeconds+'</p></div>');
+	$('div.countdown-clock').append('<div id="clock"><p><span id="daysLeft"></span> '+countdownDays+'</p><div class="space">:</div><p><span id="hours"></span> '+countdownHours+'</p><div class="space">:</div><p><span id="minutes"></span> '+countdownMinutes+'</p><div class="space">:</div><p><span id="seconds"></span> '+countdownSeconds+'</p></div>');
 
-		$('div#clock').countdown(countdownDate, function(event) {
-			$this = $(this);
-			switch(event.type) {
-				case "seconds":
-				case "minutes":
-				case "hours":
-				case "days":
-				case "weeks":
-				case "daysLeft":
-					$this.find('span#'+event.type).html(event.value);
-					break;
-				case "finished":r
-					$this.fadeTo('slow', .5);
-					break;
-			}
-		});
-	}, 0);
+	$('div#clock').countdown(countdownDate, function(event) {
+		$this = $(this);
+		switch(event.type) {
+			case "seconds":
+			case "minutes":
+			case "hours":
+			case "days":
+			case "weeks":
+			case "daysLeft":
+				$this.find('span#'+event.type).html(event.value);
+				break;
+			case "finished":r
+				$this.fadeTo('slow', .5);
+				break;
+		}
+	});
 		
 	if($(window).width() > 1024) {
 		$(window).scroll(function() {
@@ -42,4 +40,4 @@
 		$('#clock-wrapper').find('h2, a').addClass('animated');
 		$('.node-fj16-frontpage-box').find('h2, .body, .link').addClass('animated');
 	}
-})(jQuery);
+});
