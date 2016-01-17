b613c4a332805ac46429f9641fc3d687c35a4505
Ville-Pekka Reponen <vpreponen@gmail.com>
2016-01-17 20:50:20 +0200

    typo

--- sites/all/themes/fj16/templates/html.tpl.php
+++ sites/all/themes/fj16/templates/html.tpl.php
@@ -87,7 +87,7 @@
   fbq('init', '1685385408370307');
   fbq('track', "PageView");</script>
   <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1685385408370307&amp:ev=PageView&amp:noscript=1"
+  src="https://www.facebook.com/tr?id=1685385408370307&amp;ev=PageView&amp;noscript=1"
   /></noscript>
   <!-- End Facebook Pixel Code -->
 
