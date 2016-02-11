773269cb59624255eeb7470b308c8ee25d60c4be
Emil Virkki <emil.virkki@gmail.com>
2016-02-11 20:11:27 +0200

    Don't run cron on deploy

--- fj16-deploy.sh
+++ fj16-deploy.sh
@@ -11,8 +11,6 @@ echo "Reverting features..."
 /var/lib/nginx/drush/drush features-revert-all -y
 echo "Clearing caches..."
 /var/lib/nginx/drush/drush cc all #clear caches
-echo "Running cron..."
-/var/lib/nginx/drush/drush core-cron
 
 hostname=$(hostname)
 flowname="web-sivut"
