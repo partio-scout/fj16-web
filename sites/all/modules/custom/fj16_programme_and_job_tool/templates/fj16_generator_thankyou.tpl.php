666320525c19bcb50e836f7c675e5c1e0a62d202
Emil Virkki <emil.virkki@gmail.com>
2016-02-14 03:17:10 +0200

    Tweak thank you page styles

--- sites/all/modules/custom/fj16_programme_and_job_tool/templates/fj16_generator_thankyou.tpl.php
+++ sites/all/modules/custom/fj16_programme_and_job_tool/templates/fj16_generator_thankyou.tpl.php
@@ -1,23 +1,27 @@
 <div class="generator-thankyou">
   <div class="job">
     <p><?php print t('Your choices have been saved into our system.'); ?></p>
-    <p><span class="label"><?php print t('Your staff position'); ?>:</span> <?php print $job_name; ?></p>
     <p>
-      <span class="label"><?php print t('Contact person'); ?>:</span>
+      <strong class="label"><?php print t('Your staff position'); ?>:</strong>
+      <?php print $job_name; ?></p>
+    <p>
+      <strong class="label"><?php print t('Contact person'); ?>:</strong>
       <?php print $contact_name; ?>,
       <?php if($contact_phone): ?>
         <?php print $contact_phone; ?>,
       <?php endif; ?>
       <?php print l($contact_email, 'mailto:' . $contact_email); ?>
+      <br>
+      <?php print $job_further_info; ?>
     </p>
-    <p><?php print $job_further_info; ?></p>
   </div>
   <?php if($will_o_the_wisp_name): ?>
     <div class="will-o-the-wisp">
-      <p><span class="label"><?php print t("Your Will-o'-the-wisp 24h off-camp adventure"); ?>:</span> <?php print $will_o_the_wisp_name; ?></p>
+      <p><strong class="label"><?php print t("Your Will-o'-the-wisp 24h off-camp adventure"); ?>:
+      </strong> <?php print $will_o_the_wisp_name; ?></p>
     </div>
   <?php endif; ?>
   <div class="further-info">
-    <?php t('See you at Roihu!'); ?>
+    <?php print t('See you at Roihu!'); ?>
   </div>
 </div>
