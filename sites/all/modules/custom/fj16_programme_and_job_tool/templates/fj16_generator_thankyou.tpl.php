640ad9b164522a6a3fd10f1122a7e5d03f919fb4
Emil Virkki <emil.virkki@gmail.com>
2016-02-14 02:49:08 +0200

    Add more localisable strings

--- sites/all/modules/custom/fj16_programme_and_job_tool/templates/fj16_generator_thankyou.tpl.php
+++ sites/all/modules/custom/fj16_programme_and_job_tool/templates/fj16_generator_thankyou.tpl.php
@@ -1,8 +1,9 @@
 <div class="generator-thankyou">
   <div class="job">
-    <p><span class="label"><?php print t('Your assignment'); ?>:</span> <?php print $job_name; ?></p>
+    <p><?php print('Your choices have been saved into our system and a confirmation email has been sent.); ?></p>
+    <p><span class="label"><?php print t('Your staff position'); ?>:</span> <?php print $job_name; ?></p>
     <p>
-      <span class="label"><?php print t('Assignment contact'); ?>:</span>
+      <span class="label"><?php print t('Contact person'); ?>:</span>
       <?php print $contact_name; ?>,
       <?php if($contact_phone): ?>
         <?php print $contact_phone; ?>,
@@ -13,10 +14,10 @@
   </div>
   <?php if($will_o_the_wisp_name): ?>
     <div class="will-o-the-wisp">
-      <p><span class="label"><?php print t("Your will-o'-the-wisp"); ?>:</span> <?php print $will_o_the_wisp_name; ?></p>
+      <p><span class="label"><?php print t('Your Will-o\'-the-wisp 24h off-camp adventure'); ?>:</span> <?php print $will_o_the_wisp_name; ?></p>
     </div>
   <?php endif; ?>
   <div class="further-info">
-    <?php print $info_text; ?>
+    <?php t('See you at Roihu!'); ?>
   </div>
 </div>
