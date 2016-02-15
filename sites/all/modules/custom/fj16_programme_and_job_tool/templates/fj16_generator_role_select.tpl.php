e1542434e797fd9ac95c822131be20f8b8eb9ba5
Emil Virkki <emil.virkki@gmail.com>
2016-02-15 16:05:41 +0200

    Add intro text to role selection

---
+++ sites/all/modules/custom/fj16_programme_and_job_tool/templates/fj16_generator_role_select.tpl.php
@@ -0,0 +1,74 @@
+<div class="role-select-intro">
+  <?php if($current_lang === 'fi'): ?>
+    <p>
+      Tervetuloa valitsemaan itsellesi pestiä Roihulla. Aivan ensimmäiseksi kuvittele itsesi seuraavassa työhaastattelussasi. Mitä haluat kertoa Roihulla tehneesi?
+    </p>
+    <p>
+      Voit rajata pestejä niiden ominaisuuksien perusteella, valitse sieltä jotain mukavaa ja jotain haastavaa.
+    </p>
+    <p>
+      Kehitin on auki toistaiseksi, parhaat pestit varataan nyt. Kesäkuussa on jo aivan liian myöhäistä.
+    </p>
+    <p>
+      Jos luit ohjeet tänne asti (ja olet vaeltaja tai aikuinen) saatat olla priimaa johtaja-ainesta. Roihulle etsitään vielä myös Mestari- ja Kymppitason tekijöitä, nämä pestit löytyvät osoitteesta <a href="http://roihu2016.fi/pestit">http://roihu2016.fi/pestit</a>. Sivulta löytyy lisäksi sellaiset tekijätason pestit, joissa vaaditaan erityisosaamista, kannattaa kurkata läpi!
+    </p>
+    <p>
+      Mikäli sinulla on kysyttävää, lähetä postia <a href="mailto:kehitin@roihu2016.fi">kehitin@roihu2016.fi</a> tai soita Pestikehitin tukeen +358 50 482 1683 (Mikko).
+    </p>
+    <h4>Samoajille</h4>
+    <p>
+      Samoajien leirikokemus koostuu useasta toisiinsa sopivasta palasta. Ensin kannattaa valita itseään eniten houkuttava Virvatuli, eli 24h kokemus leiriarjen ulkopuolella. Virvatulet esitellään sinulle seuraavaksi. Leiri antaa ja ottaa myös pestin muodossa, samoajien pestit kestävät noin kahta päivää vastaavan ajan. Pesti valitaan heti Virvatulen jälkeen ja valikoimaa on tarjolla reilusti. Jos haluat samaan pestiin kaverisi kanssa teidän kannattaa suorittaa pestin valinta yhdessä.
+    </p>
+    <p>
+      Virvatuliin liittyvät kysymykset voi lähettää osoitteeseen <a href="mailto:kehitin@roihu2016.fi">kehitin@roihu2016.fi</a>.
+    </p>
+
+  <?php endif; ?>
+  <?php if($current_lang === 'sv'): ?>
+    <p>
+      Välkommen att välja serviceuppdrag för lägertiden. Först av allt skall du föreställa dig att du sitter på din följande arbetsintervju. Vad vill du berätta att du gjort och varit med om inom scoutingen och på Roihu? 
+    </p>
+    <p>
+      Du kan begränsa de uppdrag som visas enligt olika egenskaper. Välj något trevligt och lämpligt utmanande. 
+    </p>
+    <p>
+      Uppdragsgeneratorn är öppen tillsvidare, men de bästa uppdragen reserveras nu. Du är nog för sent ute i juni. 
+    </p>
+    <p>
+      Om du läste instruktionerna så här långt och du är äldre ledare / vuxen eller roverscout tror vi att du skulle passa bra i ett mer krävande uppdrag. Det finns ännu lediganslagna bas- och mästaruppdrag på <a href="http://roihu2016.fi/sv/pestit">http://roihu2016.fi/sv/pestit</a>.
+    </p>
+    <p>
+      Om du har frågor, kan du skicka e-post till <a href="mailto:kehitin@roihu2016.fi">kehitin@roihu2016.fi</a> eller ringa till Uppdragsgeneratorns helpdesk +358 50 482 1683 (Mikko).
+    </p>
+    <h4>Explorerscouter</h4>
+    <p>
+      Explorerscouternas lägerupplevelse består av flere delar som är anpassade till varandra. Till först lönar det sig att välja den löpeld, som intresserar dig mest. Löpelden är en 24 h upplevelse som bryter den normala lägervardagen. Löpeldarna presenteras till nästa. Lägret både ger och tar i form av serviceuppdragena; längden på explorerscouternas serviceuppdrag motsvarar ca. 2 lägerdagar. Du väljer uppdrag genast efter att du valt löpeld. Utbudet av olika slags uppdrag är stort! Om du önskar  samma uppdrag som din kompis, lönar det sig att välja uppdraget tillsammans. Frågor gällande löpeldarna kan du skicka per e-post till <a href="mailto:kehitin@roihu2016.fi">kehitin@roihu2016.fi</a>.
+    </p>
+  <?php endif; ?>
+  <?php if($current_lang === 'en'): ?>
+    <p>
+      Welcome to the Generator where you can pick a staff position for yourself for Roihu. First, picture yourself in your next job interview. What kind of things would you like to be able to say you did at the camp? 
+    </p>
+    <p>
+      You can use this criteria to narrow down the staff positions, or simply choose something nice or challenging. 
+    </p>
+    <p>
+      The Generator will remain open for now, but the best staff positions will be filled quickly. You will be too late if you wait until June. 
+    </p>
+    <p>
+      If you have read the instructions so far (and you are a Rover or an adult), you might be a prime candidate for a leadership position. We are still looking for team and sub team leaders for Roihu so check out the staff positions at <a href="http://roihu2016.fi/en/pestit">http://roihu2016.fi/en/pestit</a>.
+    </p>
+    <p>
+      If you have any questions, please send an email to <a href="mailto:kehitin@roihu2016.fi">kehitin@roihu2016.fi</a> or call Generator Helpline at +358 50 482 1683 (Mikko will take your call). 
+    </p>
+    <h4>For Explorers</h4>
+    <p>
+      For Explorers, the camp experience will include various parts that complement each other. First, pick the most interesting Will-o'-the-Wisp, a 24-hour experience outside the main camp. You will be presented with different options for Will-o'-the-Wisp next. The camp is about giving and getting, which applies also to your staff position. Explorer-level staff positions require approximately 2 days worth of work. You will choose your staff position straight after the Will-o'-the-Wisp and there will be plenty to choose from. If you'd like to have the same position as your friend, make sure to choose the staff positions together. 
+    </p>
+    <p>
+      If you have any questions regarding the Will-o'-the-Wisp, please contact <a href="mailto:kehitin@roihu2016.fi">kehitin@roihu2016.fi</a>. 
+    </p>
+
+  <?php endif; ?>
+
+</div>
