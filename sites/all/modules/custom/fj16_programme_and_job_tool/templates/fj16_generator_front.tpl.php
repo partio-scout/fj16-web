0d0c6a9ecf332298ce8af0af9f1635335d2188f2
Emil Virkki <emil.virkki@gmail.com>
2016-02-15 16:48:59 +0200

    Hide lorem ipsum and style login page nicer

--- sites/all/modules/custom/fj16_programme_and_job_tool/templates/fj16_generator_front.tpl.php
+++ sites/all/modules/custom/fj16_programme_and_job_tool/templates/fj16_generator_front.tpl.php
@@ -1,5 +1,4 @@
 <div class="generator-login">
-  <p><?php print $intro_text; ?></p>
-  <p><?php print $login_link; ?></p>
+  <p class="scoutid-login"><?php print $login_link; ?></p>
   <p><?php print $alternative_login_link; ?></p>
 </div>
